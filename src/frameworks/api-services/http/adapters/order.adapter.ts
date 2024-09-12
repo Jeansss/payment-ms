b3513d7ac3d8acbf93b1e68bb0339721cb9c8877
Yuri Cordeiro <yuricordeiroc@hotmail.com>
2024-09-12 00:17:16 -0300

    fix url call

--- src/frameworks/api-services/http/adapters/order.adapter.ts
+++ src/frameworks/api-services/http/adapters/order.adapter.ts
@@ -10,27 +10,17 @@ export class OrderAdapter implements IOrderPort {
     constructor(private readonly httpService: HttpService) { }
 
     getCartById(cartId: string): Promise<AxiosResponse<Cart>> {
-        const localURL = `http://0.0.0.0:3001/carts/id/${cartId}`;
-        const containerURL = `http://order_ms:3001/carts/id/${cartId}`;
+        const finalUrl = `http://af2656d4febb4451d86617b0e544401e-1306484774.us-east-1.elb.amazonaws.com/carts/id/${cartId}`;
 
         return this.httpService.
-            axiosRef.get(localURL)
-            .catch(() => {
-                return this.httpService.
-                    axiosRef.get(containerURL)
-            });
+            axiosRef.get(finalUrl);
     }
 
     addTransactionToCart(cartId: string, transactionId: string): Promise<AxiosResponse> {
-        const localURL = `http://0.0.0.0:3001/carts/${cartId}/transactions/${transactionId}`;
-        const containerURL = `http://order_ms:3001/carts/${cartId}/transactions/${transactionId}`;
+        const finalUrl = `http://af2656d4febb4451d86617b0e544401e-1306484774.us-east-1.elb.amazonaws.com/carts/${cartId}/transactions/${transactionId}`
 
         return this.httpService.
-            axiosRef.put(localURL)
-            .catch(() => {
-                return this.httpService.
-                    axiosRef.get(containerURL)
-            });
+            axiosRef.put(finalUrl);
     }
 
 }
